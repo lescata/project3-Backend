c66f60663f84132deeeffe09b83e8c7bacc56abe
lescata <alexandre.lescat@gmail.com>
2022-10-03 21:26:09 +0200

    login par Alex

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const saltRounds = 7;
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
@@ -69,5 +70,46 @@ User.findOne({ email })
 //  ╦  ┌─┐┌─┐┌─┐┬┌┐┌
 //  ║  │ ││ ┬│ ┬││││
 //  ╩═╝└─┘└─┘└─┘┴┘└┘
+
+router.post('/sessions', (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Verification de la presence de l'email et du password dans le formulaire
+  if (email === ''|| password === ''){
+    res.status(400).json({message:"Please provide Both Email & Password."});
+    return;
+  }
+ //Verification de l'utilisateur (email) dans la Db
+ User.findOne({email})
+ .then((foundUser) => {
+
+  if(!foundUser){
+    res.status(401).json({message: "User could not be found."});
+    return;
+  }
+  //comparaison du password recu vs celui dans la DB 
+const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+if (passwordCorrect) {
+const { _id, email} = foundUser;
+
+const paylaod = {_id, email};
+
+const authKoen = jwt.sign(
+  payload,
+  process.env.TOKEN_SECRET,
+  {algorithm: 'HS256', expiresIn:'5h'}
+  );
+
+  res.status(200).json({authToken: authToken});
+}
+else {
+  res.status(401).json({message: "Unable to authenticate the user"});
+}
+ })
+.catch(err => res.status(500).json({message:"Internal Server Error"}));
+
+});
+
 
 module.exports = router;
